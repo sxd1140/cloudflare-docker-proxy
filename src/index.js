a67022189b41a82b7e5413ea46576f75d9ebdbf0
Sam Sun <sxd1140@aliyun.com>
2024-07-14 20:53:02 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.mirror.606422.xyz": dockerHub,
+  "quay.mirror.606422.xyz": "https://quay.io",
+  "gcr.mirror.606422.xyz": "https://gcr.io",
+  "k8s-gcr.mirror.606422.xyz": "https://k8s.gcr.io",
+  "k8s.mirror.606422.xyz": "https://registry.k8s.io",
+  "ghcr.mirror.606422.xyz": "https://ghcr.io",
+  "cloudsmith.mirror.606422.xyz": "https://docker.cloudsmith.io",
+  "ecr.mirror.606422.xyz": "https://public.ecr.aws",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.mirror.606422.xyz": dockerHub,
 };
 
 function routeByHosts(host) {
